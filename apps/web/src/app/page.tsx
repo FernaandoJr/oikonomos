0fa8ce1cd0d419a4b550d1258e97341349a446f1
FernaandoJr <fernaando.divino@gmail.com>
2026-02-18 21:30:07 -0300

    chore: remove my-project app and update dependencies in the web app

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { CTASection } from "@/components/hero-dithering-card"
+
+export default function Home() {
+	return (
+		<div className="flex min-h-screen items-center justify-center">
+			<CTASection />
+			<p>oiKonomos</p>
+		</div>
+	)
+}
